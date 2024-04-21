57ea3d7903ff1a5250478d2e41fb4a1fb57c3eb4
Nikos Daridis <nikosdaridis@gmail.com>
2024-04-21 03:26:20 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,21 @@
 #!/bin/sh
 curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
 chmod +x dotnet-install.sh
+
 ./dotnet-install.sh -c 8.0 -InstallDir ./dotnet
 ./dotnet/dotnet --version
+
+if ! command -v node &> /dev/null
+then
+    curl -sL https://deb.nodesource.com/setup_20.x | bash -
+    apt-get install -y nodejs
+fi
+
+node -v
+npm -v
+
+npm install
+npm install -g typescript
+tsc -v
+
 ./dotnet/dotnet publish -c Release -o output
